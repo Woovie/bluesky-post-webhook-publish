1c4118dc6a91b24066c636af0df6fdeb70ccca7a
Jordan Banasik <woovie@woovie.net>
2023-08-19 13:13:12 -0700

    Add variable defining environment

--- terraform/production/generics/azure-init.tf
+++ terraform/production/generics/azure-init.tf
@@ -12,6 +12,11 @@ variable "SUBSCRIPTION_ID" {
   description = "Azure Subscription ID"
 }
 
+variable "ENVIRONMENT" {
+  default = "prod"
+  description = "Deployment environment"
+}
+
 provider "azurerm" {
   # skip_provider_registration = true # This is only required when the User, Service Principal, or Identity running Terraform lacks the permissions to register Azure Resource Providers.
   features {}
